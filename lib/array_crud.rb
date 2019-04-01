3a99a07d6732a9a91887be343f18a90679ed8915
brindab50 behal <brindab50@gmail.com>
2019-04-01 16:32:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["wow", "fruit", "vegatable", "meat", "grain", "arrays!",]
+  food =  ["wow", "fruit", "vegatable", "meat", "grain", "arrays!",]
   food[2]
 end
 
