845c2c5393305d09a6bc55011c69b91eb224b460
brindab50 behal <brindab50@gmail.com>
2019-04-01 16:28:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-    food = ["wow", "fruit", "vegatable", "meat", "grain", "arrays!",]
+    food = ["fruit", "vegatable", "meat", "grain", "arrays!",]
     arrays = food.pop
 end
 
