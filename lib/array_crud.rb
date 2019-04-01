c7a3abe542ac7d408ace43d9e09cc74c053e9cb3
brindab50 behal <brindab50@gmail.com>
2019-04-01 16:35:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,5 +40,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  food =  ["wow", "fruit", "am", "meat", "grain", "arrays!",]
+    food.last 
 end
